96b1d0904890b773b5fd9b23ec1e78d01d29e510
alexo75 <alexander.oneill75@gmail.com>
2024-01-28 21:01:38 -0500

    idk man

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let y;
+  let tabs = [
+    {name: 'Projects', link:'#projects'},
+    {name: 'About Me', link:'#about'}
+    // {name: 'Projects', '#projects'},
+  ]
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-solid ' + (
+    y > 0 ? ' py-4 bg-slate-950 border-violet-950 ': ' py-6 bg-transparent border-transparent '
+)}>
+    <h1>
+        <b>Alexander</b> O'Neill
+    </h1>
+    <div class="">
+        {#each tabs as tab, index}
+        <a href="">
+            <p>{tab.name}</p>
+        </a>
+            
+        {/each}
+    </div>
+</header>

8e80f5d41b430163ed29f0c798b534b71d32cf02
alexo75 <alexander.oneill75@gmail.com>
2024-01-29 23:12:12 -0500

    Quickpush
    added some content to the main section of the page.
    
    looking ahead, its worth thinking about how I can make this page stand out a little more, (implement other ideas from online etc)

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,21 @@
-alex
+<script>
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id = "introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class ="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi, I'm <span class="text-violet-400">Alexander</span> O'Neill <br/> Full Stack Developer
+                <span class="poppins text-violet-400"> Developer</span>\
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                I'm a fledgling full stack developer, learning svelte with experience in a wide variety of languages.
+                I'm currently a student at the University of Southern Maine studying Computer Science, slated to graduate in the spring 2024.
+            </p>
+
+        </div>
+
+    </section>
+
+</main>

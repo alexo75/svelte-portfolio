5a4b500010d51fceb26594e400b6b81878de264d
alexo75 <alexander.oneill75@gmail.com>
2024-01-30 19:32:38 -0500

    small change
    might do more later, but want to get something in if I dont get back til l8

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,8 +6,8 @@
     <section id = "introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
         <div class ="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi, I'm <span class="text-violet-400">Alexander</span> O'Neill <br/> Full Stack Developer
-                <span class="poppins text-violet-400"> Developer</span>\
+                Hi, I'm <span class=" poppins text-blue-500">Alexander</span> O'Neill <br/> Full Stack
+                <span class="poppins text-blue-500"> Developer</span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
                 I'm a fledgling full stack developer, learning svelte with experience in a wide variety of languages.
@@ -15,6 +15,17 @@
             </p>
 
         </div>
+
+        <div>
+
+        </div>
+        <p>
+
+        </p>
+
+        <style>
+
+        </style>
 
     </section>
 

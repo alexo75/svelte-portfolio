9f04d4eb5970f5bb9d1afe93ced934105ab7e904
alexo75 <alexander.oneill75@gmail.com>
2024-02-01 18:06:35 -0500

    So far so good
    cleaning up, addded a photo. Almost done with the main layout

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,37 +1,71 @@
 <script>
-
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section id = "introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
-        <div class ="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
-            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi, I'm <span class=" poppins text-blue-500">Alexander</span> O'Neill <br/> Full Stack
-                <span class="poppins text-blue-500"> Developer</span>
-            </h2>
-            <p class="text-base sm:text-lg md:text-xl">
-                I'm a fledgling full stack developer, learning svelte with experience in a wide variety of languages.
-                I'm currently a student at the University of Southern Maine studying Computer Science, slated to graduate in the spring 2024.
-            </p>
-            <p>
-                Some of my favorite langauges/frameworks include:
-                JavaScript, Svelte, React, Python, Elixir, 
-            </p>
+  <section
+    id="introPage"
+    class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+  >
+    <div
+      class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+    >
+      <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi there! <br>
+         I'm <span class=" poppins text-blue-500">Alexander</span> O'Neill
+        <br />
+        Full Stack
+        <span class="poppins text-blue-500"> Developer</span>
+      </h2>
+      <p class="text-base sm:text-lg md:text-xl">
+        I'm a fledgling full stack developer, learning SvelteKit with experience in
+        a wide variety of languages. I'm currently a student at the University
+        of Southern Maine studying Computer Science, slated to graduate in the
+        spring 2024.
+      </p>
+      <p>
+        Some of my favorite langauges/frameworks to use include:<br /> <span class="poppins text-blue-500">
+        JavaScript, Svelte, React/React Native, Python, Elixir, Java, and C++.</span>
+      </p>
 
-        </div>
+      <a
+        class="blueShadow ,x-autp lg:mr-auto text-base sm:text-lg md:text-xl
+         px-5 py-2 group rounded-full bg-white text-slate-500 cursor-pointer"
+        href="https://www.linkedin.com/in/alexandermoneill/"
+        target="_blank"
+      >
+        <div
+          class="absolute top-0 right-full w-full h-full bg-blue-500
+          opacity-20 group-hover:translate-x-full z-0 duration-200"
+        ></div>
+        <h4 class="relative z-9">Reach out</h4>
+      </a>
+    </div>
+    <div class="relative shadow-2xl grid place-items-center">
+      <img
+        src={"images/nobggoober.png"}
+        alt="profile picture"
+        class="object-cover z-[2] max-b[70vh] rounded-md"
+      />
+    </div>
 
-        <div>
+    <style>
+    </style>
+  </section>
 
-        </div>
-
-        <p>
-
-        </p>
-
-        <style>
-
-        </style>
-
-    </section>
-
+  <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-4 text-center">
+      <h6 class="text-xl underline sm:text-3xl md:text-4xl ">Projects</h6>
+      <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+        Check out some of my favorite <span class="poppins text-blue-500">projects</span>.
+      </h3>
+    </div>
+    <a
+      href="https://github.com/alexo75"
+      target="_blank"
+      class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex
+    items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-blue-800 duration-200"
+    >
+      <i class="fa-brands fa-github fa-3x" />
+    </a>
+  </section>
 </main>
